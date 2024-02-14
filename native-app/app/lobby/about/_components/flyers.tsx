4c0b45f4b1625ea0bb813392da6030edf2b42b99
Octtavo <octtavo9@gmail.com>
2024-02-14 14:00:56 -0300

    feat: add Flyers components

---
+++ native-app/app/lobby/about/_components/flyers.tsx
@@ -0,0 +1,36 @@
+import { Button } from "@/app/_components/ui/button";
+import { Card, CardContent } from "@/app/_components/ui/card";
+import { Plan } from "@prisma/client";
+import { UserRound } from "lucide-react";
+
+interface FlyersProps {
+  plan: Plan;
+}
+export default function Flyers({ plan }: FlyersProps) {
+  return (
+    <>
+      <Card className="bg-primary/50" key={plan.id}>
+        <CardContent className="flex flex-col justify-center px-3 py-5 gap-3 ">
+          <div className="flex justify-center w-full">
+            <span className="flex justify-center items-center w-10 h-10 bg-primary rounded-full">
+              <UserRound />
+            </span>
+          </div>
+          <div className="flex flex-col gap-2 my-6">
+            <h1 className="text-lg text-ellipsis overflow-hidden">
+              {plan.name}
+            </h1>
+            <p className="text-xs max-w-60 text-gray-400">{plan.description}</p>
+            <h2 className="mt-5">
+              {Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              }).format(Number(plan.price))}
+            </h2>
+          </div>
+          <Button>Assinar</Button>
+        </CardContent>
+      </Card>
+    </>
+  );
+}
